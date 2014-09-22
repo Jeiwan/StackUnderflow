7125a14636927f8cc3870e101ca5e51ed548a0dc
Ivan Kuznetsov <me@jeiwan.ru>
2014-09-22 15:52:14 +0400

    Delete answer comment: finished

---
+++ spec/features/comment/delete_answer_comment_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "Delete Question Comment" do
+
+  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, user:user, question: question) }
+  given!(:comment) { create(:answer_comment, user: user, commentable: answer) }
+  given!(:comment2) { create(:answer_comment, user: user2, commentable: answer) }
+
+  background do
+    sign_in user
+    visit question_path(question)
+  end
+
+  scenario "User deletes his answer comment" do
+    within(".answer[data-answer-id='#{answer.id}'] .comment[data-comment-id='#{comment.id}']") do
+      click_on "delete"
+    end
+
+    expect(page).not_to have_selector ".answers .content[data-comment-id='#{comment.id}']"
+  end
+
+  scenario "User can't delete not his answer comment" do
+    within(".answer[data-answer-id='#{answer.id}'] .comment[data-comment-id='#{comment2.id}']") do
+      expect(page).not_to have_content "delete"
+    end
+  end
+
+end
