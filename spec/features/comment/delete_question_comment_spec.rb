42ac944405559f1d6bc5ec9e4c07fd56d1aaa6b4
Ivan Kuznetsov <me@jeiwan.ru>
2014-09-22 12:54:59 +0400

    Delete question comment finished

--- spec/features/comment/delete_question_comment_spec.rb
+++ spec/features/comment/delete_question_comment_spec.rb
@@ -3,8 +3,10 @@ require "rails_helper"
 feature "Delete Question Comment" do
 
   given(:user) { create(:user) }
+  given(:user2) { create(:user) }
   given(:question) { create(:question, user: user) }
   given!(:comment) { create(:question_comment, user: user, commentable: question) }
+  given!(:comment2) { create(:question_comment, user: user2, commentable: question) }
 
   background do
     sign_in user
@@ -16,7 +18,13 @@ feature "Delete Question Comment" do
       click_on "delete"
     end
 
-    expect(page).not_to have_content comment.body
+    expect(page).not_to have_selector ".content[data-comment-id='#{comment.id}']"
+  end
+
+  scenario "User can't delete not his question comment" do
+    within(".comment[data-comment-id='#{comment2.id}']") do
+      expect(page).not_to have_content "delete"
+    end
   end
 
 end
