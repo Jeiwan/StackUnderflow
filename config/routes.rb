d34ee746e1a3618d8e91519204cad366314845f2
Ivan Kuznetsov <me@jeiwan.ru>
2014-09-25 16:31:11 +0400

    Use concern in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :questions do
-    resources :answers, only: [:new, :create, :edit, :update, :destroy] do
-      post "mark_best", on: :member
-    end
+
+  concern :commentable do
     resources :comments, only: [:create, :edit, :update, :destroy]
   end
 
-  resources :answer, only: [] do
-    resources :comments, only: [:create, :edit, :update, :destroy]
+  resources :questions, concerns: :commentable do
+    resources :answers, only: [:new, :create, :edit, :update, :destroy], shallow: true, concerns: :commentable do
+      post "mark_best", on: :member
+    end
   end
 
   root "questions#index"
