066517f3cb299ee6fb58a9ee63c1ae586533bec2
Ivan Kuznetsov <me@jeiwan.ru>
2014-09-30 16:41:55 +0400

    Question model with tags: green test

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :user
   has_many :comments, as: :commentable
+  has_and_belongs_to_many :tags
 
   validates :body, presence: true, length: { in: 10..5000 }
   validates :title, presence: true, length: { in: 5..512 }
