72a2c70c7526737188eba1f89856bbc59d5a2899
Ivan Kuznetsov <me@jeiwan.ru>
2014-09-30 15:21:33 +0400

    Tag model: green test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ActiveRecord::Base
+  has_and_belongs_to_many :questions
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { in: 1..24 }, format: { with: /\A[a-zA-Z][\w_\.-]*[a-zA-Z0-9#\+]+\z/, message: "can contain only letters, digits, ., +, -, _, and #. It must begin with a letter." }
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Tag, :type => :model do
 
   describe "validations" do
     it { is_expected.to validate_presence_of :name  }
-    it { is_expected.to ensure_length_of(:name).is_at_least(2).is_at_most(24)  }
+    it { is_expected.to ensure_length_of(:name).is_at_least(1).is_at_most(24)  }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive  }
+    it { is_expected.to allow_value("tag", "TAG", "C++", "some_tag", "some-tag", "a123", "android-4.2").for :name  }
+    it { is_expected.not_to allow_value("t a g", "123", "123a", "a123").for :name  }
   end
 end
