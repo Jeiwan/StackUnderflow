3ce89dfc855afe27fc87e530f344fc1e4109bb61
Ivan Kuznetsov <me@jeiwan.ru>
2014-10-19 23:51:24 +0400

    Refactor Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,26 @@ class CommentsController < ApplicationController
   before_action :find_comment, only: [:edit, :update, :destroy]
   before_action :comment_belongs_to_current_user?, only: [:edit, :update, :destroy]
   after_action :publish_after_destroy, only: [:destroy]
+  after_action :publish_after_create, only: [:create]
+
+  respond_to :json
 
   def create
-    @comment = @parent.comments.new(comment_params)
-
-    if @comment.save
-      current_user.comments << @comment
-      publish_on_create
-      render json: @comment, status: 201, root: false
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+    respond_with @comment = @parent.comments.create(comment_params.merge(user_id: current_user.id))
   end
 
   def update
-    update_resource @comment
+    respond_with @comment.update(comment_params) do |format|
+      if @comment.valid?
+        format.json { render json: @comment, status: 200 }
+      else
+        format.json { render json: @comment.errors, status: 422 }
+      end
+    end
   end
 
   def destroy
-    destroy_resource @comment
+    respond_with @comment.destroy
   end
 
   private
@@ -44,7 +45,7 @@ class CommentsController < ApplicationController
       PrivatePub.publish_to "/questions/#{@comment.commentable.class.name == 'Question' ? @comment.commentable.id : @comment.commentable.question.id}", comment_destroy: @comment.id, parent: @comment.commentable.class.name, parent_id: @comment.commentable.id
     end
 
-    def publish_on_create
-      PrivatePub.publish_to "/questions/#{@parent.class.name == 'Question' ? @parent.id : @parent.question.id}", comment_create: CommentSerializer.new(@comment, root: false).to_json, parent: @parent.class.name, parent_id: @parent.id
+    def publish_after_create
+      PrivatePub.publish_to "/questions/#{@parent.class.name == 'Question' ? @parent.id : @parent.question.id}", comment_create: CommentSerializer.new(@comment, root: false).to_json, parent: @parent.class.name, parent_id: @parent.id if @comment.valid?
     end
 end
