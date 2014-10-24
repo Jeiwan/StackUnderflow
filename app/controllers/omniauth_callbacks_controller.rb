9a4bca677c0029d3c16530d65f5c67446c12d909
Ivan Kuznetsov <me@jeiwan.ru>
2014-10-24 15:59:06 +0400

    Refactor OmniauthCallbacksController

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,25 +1,22 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :auth_provider
+
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message :notice, :success, kind: 'Facebook' if is_navigational_format?
-    end
   end
 
   def twitter
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message :notice, :success, kind: 'Twitter' if is_navigational_format?
-    end
   end
 
   def vkontakte
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message :notice, :success, kind: 'vKontekte' if is_navigational_format?
-    end
   end
+
+  private
+    
+    def auth_provider
+      @user = User.find_for_oauth(request.env['omniauth.auth'])
+      if @user
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message :notice, :success, kind: params[:action].capitalize if is_navigational_format?
+      end
+    end
 end
